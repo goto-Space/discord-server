a90aa24364d1d1c6aac16a2847af3a4c56dac403
손병재 <byeongjae.dev@gmail.com>
2023-06-06 18:40:36 +0900

    BREAKINGCHANGE: api url 변경

--- src/main/java/com/example/simple_chatting/web/UserController.java
+++ src/main/java/com/example/simple_chatting/web/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/user/")
+@RestController("/api/users/")
 @RequiredArgsConstructor
 public class UserController {
 
