19c621c01d3242446cc7dcf692261a65bd015dbf
손병재 <byeongjae.dev@gmail.com>
2023-06-07 02:55:33 +0900

    BREAKING CHANGE: api url 변경

--- src/main/java/com/example/simple_chatting/web/ChannelController.java
+++ src/main/java/com/example/simple_chatting/web/ChannelController.java
@@ -20,9 +20,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/channels/")
+@RestController
+@RequestMapping("/api/channels/")
 @RequiredArgsConstructor
 public class ChannelController {
     private final ChannelService channelService;
