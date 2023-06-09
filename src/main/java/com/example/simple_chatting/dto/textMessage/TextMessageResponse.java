3c550fb21303cd985ca4966ddcd98fa8b1d84521
손병재 <byeongjae.dev@gmail.com>
2023-06-10 00:43:06 +0900

    Refactor: use static factory method

--- src/main/java/com/example/simple_chatting/dto/textMessage/TextMessageResponse.java
+++ src/main/java/com/example/simple_chatting/dto/textMessage/TextMessageResponse.java
@@ -2,15 +2,17 @@ package com.example.simple_chatting.dto.textMessage;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 @AllArgsConstructor
 public class TextMessageResponse {
     @NotEmpty
     private String senderName;
     @NotEmpty
     private String content;
+
+    public static TextMessageResponse of(String senderName, String content) {
+        return new TextMessageResponse(senderName, content);
+    }
 }
