17a4534bf2ed754f3bb54f7d7e3bf84bcd36135d
손병재 <byeongjae.dev@gmail.com>
2023-06-10 01:34:51 +0900

    Docs: swagger 설정 적용

---
+++ src/main/java/com/example/simple_chatting/Config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.simple_chatting.config;
+
+import com.example.simple_chatting.security.LoginUser;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+    info = @Info(title = "gotoSpace API 명세서",
+        description = "gotoSpace API 명세서입니다.",
+        version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(LoginUser.class);
+    }
+}
