d5efc053f49406a820baad179bfb10069abb164e
손병재 <byeongjae.dev@gmail.com>
2023-06-13 14:40:42 +0900

    Fix: Cors 설정 변경

--- src/main/java/com/example/simple_chatting/config/WebMvcConfig.java
+++ src/main/java/com/example/simple_chatting/config/WebMvcConfig.java
@@ -33,7 +33,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+            .allowedOrigins("*")
             .allowedMethods("*");
     }
 }
